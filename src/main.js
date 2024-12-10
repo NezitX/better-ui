6a6da47eb49a5b8b9692454617951b72b14a4d6b
NezitX <xnezit@gmail.com>
2024-12-10 15:40:43 +0300

    feat: custom css file && api

--- src/main.js
+++ src/main.js
@@ -1,137 +1,186 @@
-import plugin from '../plugin.json';
+import plugin from "../plugin.json";
 
-import sidebarStyle from './components/sidebar.scss';
-import settingsStyle from './components/settings.scss';
-import checkboxStyle from './components/checkbox.scss';
-import dialogStyle from './components/dialog.scss';
-import menuStyle from './components/menu.scss';
-import pageStyle from './components/page.scss';
-import pallettesStyle from './components/pallettes.scss';
-import hintsStyle from './components/hints.scss';
+import sidebarStyle from "./components/sidebar.scss";
+import settingsStyle from "./components/settings.scss";
+import checkboxStyle from "./components/checkbox.scss";
+import dialogStyle from "./components/dialog.scss";
+import menuStyle from "./components/menu.scss";
+import pageStyle from "./components/page.scss";
+import pallettesStyle from "./components/pallettes.scss";
+import hintsStyle from "./components/hints.scss";
 
-const appSettings = acode.require('settings');
+const appSettings = acode.require("settings");
+const fs = acode.require("fs");
+const EditorFile = acode.require("EditorFile");
+const actionStack = acode.require("actionStack");
+
+const BetterUI = {};
+
+// constants
+BetterUI.UiPath = `${window.DATA_STORAGE}/ui`;
+BetterUI.CustomCssPath = `${BetterUI.UiPath}/BetterUI.custom.css`;
+BetterUI.UiTypes = [
+  "sidebar",
+  "settings",
+  "checkbox",
+  "dialog",
+  "menu",
+  "page",
+  "pallettes",
+  "hints"
+];
 
 class BetterUi {
-  static UI_TYPES = ['sidebar', 'settings', 'checkbox', 'dialog', 'menu', 'page', 'pallettes', 'hints'];
   constructor() {
     if (!this.settings) {
       appSettings.value[plugin.id] = {
-        activeTypes: BetterUi.UI_TYPES,
-        customCss: ''
+        activeTypes: BetterUI.UiTypes
       };
 
       appSettings.update(false);
-    };
-  };
+    }
+  }
 
   get settings() {
     return appSettings.value[plugin.id];
   }
 
-  init() {
-    this.$style = tag('style');
+  async init() {
+    // check if ui dir exist or not create one
+    const isUiDirExist = await fs(BetterUI.UiPath).exists();
+    if (!isUiDirExist) {
+      await fs(window.DATA_STORAGE).createDirectory("ui");
+    }
 
-    if (this.settings.customCss.trim() !== '') {
-      this.$style.textContent += this.settings.customCss.trim();
-    };
+    // check if custom css file exist or not create one
+    const isCustomCssExist = await fs(BetterUI.CustomCssPath).exists();
+    if (!isCustomCssExist) {
+      await fs(BetterUI.UiPath).createFile(
+        "BetterUI.custom.css",
+        '/* A custom css file for "Better UI" plugin */'
+      );
+    }
 
-    if (this.settings.activeTypes.includes('sidebar')) {
+    this.$style = tag("style");
+    this.$custom = tag("link", {
+      rel: "stylesheet",
+      href: await acode.toInternalUrl(
+        `${window.DATA_STORAGE}/ui/BetterUI.custom.css`
+      )
+    });
+
+    if (this.settings.activeTypes.includes("sidebar")) {
       this.$style.textContent += sidebarStyle;
-    };
+    }
 
-    if (this.settings.activeTypes.includes('settings')) {
+    if (this.settings.activeTypes.includes("settings")) {
       this.$style.textContent += settingsStyle;
-    };
+    }
 
-    if (this.settings.activeTypes.includes('checkbox')) {
+    if (this.settings.activeTypes.includes("checkbox")) {
       this.$style.textContent += checkboxStyle;
-    };
+    }
 
-    if (this.settings.activeTypes.includes('dialog')) {
+    if (this.settings.activeTypes.includes("dialog")) {
       this.$style.textContent += dialogStyle;
-    };
+    }
 
-    if (this.settings.activeTypes.includes('menu')) {
+    if (this.settings.activeTypes.includes("menu")) {
       this.$style.textContent += menuStyle;
-    };
+    }
 
-    if (this.settings.activeTypes.includes('page')) {
+    if (this.settings.activeTypes.includes("page")) {
       this.$style.textContent += pageStyle;
-    };
+    }
 
-    if (this.settings.activeTypes.includes('pallettes')) {
+    if (this.settings.activeTypes.includes("pallettes")) {
       this.$style.textContent += pallettesStyle;
-    };
-    
-    if (this.settings.activeTypes.includes('hints')) {
-      this.$style.textContent += hintsStyle;
-    };
-    
+    }
 
-    document.head.append(this.$style);
-  };
+    if (this.settings.activeTypes.includes("hints")) {
+      this.$style.textContent += hintsStyle;
+    }
+
+    document.head.append(this.$style, this.$custom);
+  }
 
   destroy() {
     this.$style.remove();
+    this.$custom.remove();
 
     delete appSettings.value[plugin.id];
     appSettings.update(true);
-  };
+  }
 
   getSettingsList() {
-    return [{
-      key: 'customCss',
-      text: 'Add Custom Css',
-      value: this.settings.customCss,
-      info: 'Make your imaginations real.\nPlease edit content in sprate place and paste it here for better control.\n(NOTE: be careful this may broke the app)',
-      prompt: 'Add Custom Css',
-      promptType: 'textarea',
-      promptOptions: {
-        placeholder: '* {\n color: red \n}; \n\n...',
-      }
-    }, ...BetterUi.UI_TYPES.map(type => ({
-      key: type,
-      text: this.#firstUpperCase(type),
-      checkbox: this.settings.activeTypes.includes(type)
-    }))];
-  };
+    return [
+      {
+        key: "customCss",
+        text: "Add Custom Css",
+        info: "Make your imaginations real.\nPlease edit content in sprate place and paste it here for better control.\n(NOTE: be careful this may broke the app)"
+      },
+      ...BetterUI.UiTypes.map(type => ({
+        key: type,
+        text: firstUpperCase(type),
+        checkbox: this.settings.activeTypes.includes(type)
+      }))
+    ];
+  }
 
-  #firstUpperCase(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-  };
+  async onSettingsChange(key, value) {
+    if (key === "customCss") {
+      const editorFile = new EditorFile("BetterUI.custom.css", {
+        uri: `${window.DATA_STORAGE}/ui/BetterUI.custom.css`
+      });
 
-  onSettingsChange(key, value) {
-    if (key === 'customCss') {
-      this.settings.customCss = value;
+      editorFile.on("save", async () => {
+        console.log("test");
+        await this.resetUi();
+      });
+
+      actionStack.pop(actionStack.length);
     } else if (value) {
       this.settings.activeTypes.push(key);
     } else {
-      this.settings.activeTypes.splice(this.settings.activeTypes.indexOf(key), 1);
-    };
+      this.settings.activeTypes.splice(
+        this.settings.activeTypes.indexOf(key),
+        1
+      );
+    }
 
-    this.resetUi();
+    await this.resetUi();
     appSettings.update(true);
-  };
+  }
 
-  resetUi() {
+  async resetUi() {
     this.$style.remove();
-    this.init();
-  };
-};
+    this.$custom.remove();
+
+    await this.init();
+  }
+}
+
+function firstUpperCase(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
 
 if (window.acode) {
-  const acodePlugin = new BetterUi();
+  const betterUI = new BetterUi();
+
+  BetterUi.resetUi = async () => await betterUI.resetUi();
+  acode.define("@better/ui", BetterUI);
+
   acode.setPluginInit(
     plugin.id,
     async (baseUrl, $page, cache) => {
-      acodePlugin.baseUrl = !baseUrl.endsWith('/') ? baseUrl += '/' : baseUrl;
-      await acodePlugin.init($page, cache.cacheFile, cache.cacheFileUrl);
+      betterUI.baseUrl = !baseUrl.endsWith("/") ? (baseUrl += "/") : baseUrl;
+      await betterUI.init($page, cache.cacheFile, cache.cacheFileUrl);
     },
     {
-      list: acodePlugin.getSettingsList(),
-      cb: acodePlugin.onSettingsChange.bind(acodePlugin)
+      list: betterUI.getSettingsList(),
+      cb: betterUI.onSettingsChange.bind(betterUI)
     }
   );
 
-  acode.setPluginUnmount(plugin.id, () => acodePlugin.destroy());
-};
+  acode.setPluginUnmount(plugin.id, async () => await betterUI.destroy());
+}
