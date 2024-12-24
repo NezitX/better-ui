e2083148a2ec5fe53e06a09fd6ca3a7de6e8ae90
NezitX <xnezit@gmail.com>
2024-12-24 16:07:36 +0300

    rename: components -> styles

---
+++ src/styles/index.js
@@ -0,0 +1,23 @@
+import sidebarStyle from "./sidebar.scss";
+import settingsStyle from "./settings.scss";
+import checkboxStyle from "./checkbox.scss";
+import dialogStyle from "./dialog.scss";
+import menuStyle from "./menu.scss";
+import pageStyle from "./page.scss";
+import pallettesStyle from "./pallettes.scss";
+import hintsStyle from "./hints.scss";
+import pluginStyle from "./plugin.scss";
+import quickToolsStyle from "./quicktools.scss";
+
+export default {
+  sidebar: sidebarStyle,
+  settings: settingsStyle,
+  checkbox: checkboxStyle,
+  dialog: dialogStyle,
+  menu: menuStyle,
+  page: pageStyle,
+  pallettes: pallettesStyle,
+  hints: hintsStyle,
+  plugin: pluginStyle,
+  quicktools: quickToolsStyle
+};
